a2a031cfae2ade2f7d79e07f59c3a0a62929eb32
LGROW101 <Chaloemrat.he@gmail.com>
2024-06-24 16:49:25 +0700

    add profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ProjectsSection from "./components/ProjectsSection";
 import TechIcons from "./components/TechIcons";
 import PersonalInfo from "./components/PersonalInfo";
@@ -21,34 +21,42 @@ export default function Home() {
     let ticker = setInterval(() => {
       tick();
     }, delta);
-
+  
     return () => { clearInterval(ticker) };
-  }, [text])
+  }, [text, delta]); 
 
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+ const tick = useCallback(() => {
+  let i = loopNum % toRotate.length;
+  let fullText = toRotate[i];
+  let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+  setText(updatedText);
 
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
+  if (isDeleting) {
+    setDelta(prevDelta => prevDelta / 2);
   }
+
+  if (!isDeleting && updatedText === fullText) {
+    setIsDeleting(true);
+    setIndex(prevIndex => prevIndex - 1);
+    setDelta(period);
+  } else if (isDeleting && updatedText === '') {
+    setIsDeleting(false);
+    setLoopNum(loopNum + 1);
+    setIndex(1);
+    setDelta(500);
+  } else {
+    setIndex(prevIndex => prevIndex + 1);
+  }
+}, [loopNum, isDeleting, text, toRotate, period]);
+
+useEffect(() => {
+  let ticker = setInterval(() => {
+    tick();
+  }, delta);
+
+  return () => { clearInterval(ticker) };
+}, [tick, delta]);
 
   return (
     <div className={darkMode ? "dark" : ""}>
