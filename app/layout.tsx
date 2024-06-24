82e2c0b341f1c362aa70aaef6f3884618ccb9ea7
LGROW101 <Chaloemrat.he@gmail.com>
2024-06-24 16:51:22 +0700

    add profile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { DarkModeProvider } from "./contexts/DarkModeContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
